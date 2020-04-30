e2ea52fad9aa42867d93426659a8418cf5763305
andrewsarras <42850602+andrewsarras@users.noreply.github.com>
2020-04-30 12:52:46 -0700

    Update rainfall_simulator.ino
    Added workable code with map function.

--- rainfall_simulator.ino
+++ rainfall_simulator.ino
@@ -1,12 +1,18 @@
 // Rainfall Simulator
-// Author:
+// Author:Reagan,Andrew
 
-void setup(){
+int potPin = 0;    // select the input pin for the potentiometer
+int waterPin = 13;   // select the pin for the LED
+int val = 0;       // variable to store the value coming from the sensor
 
-
+void setup() {
+  pinMode(waterPin, OUTPUT);  // declare the ledPin as an OUTPUT
 }
 
-void loop(){
-
-
+void loop() {
+  val = map(analogRead(potPin),0,1023,0,5000);    // read the value from the sensor
+  digitalWrite(waterPin, HIGH);  // turn the ledPin on
+  delay(val);                  // stop the program for some time
+  digitalWrite(waterPin, LOW);   // turn the ledPin off
+  delay(val);                  // stop the program for some time
 }
